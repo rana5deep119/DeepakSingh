b9e2b883748aa6322b8c6ad00799220376a280b7
rana5deep119 <dr1332616@gmail.com>
2025-08-25 14:32:31 +0530

    connection

---
+++ connectDB/connectDB.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+
+const connectdb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to MongoDB");
+    }
+    catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+module.exports ={connectdb}
